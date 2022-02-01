16d62c7353ef77542c8d50d8cf36d5f504f7e80b
mmarolin <matija.marolin11@gmail.com>
2022-02-01 22:34:17 +0100

    nav-bar-hover

--- script.js
+++ script.js
@@ -6,6 +6,11 @@ const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 const btnScrollTo = document.querySelector('.btn--scroll-to');
 const section1 = document.querySelector('#section--1');
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+const navLinks = document.querySelector('.nav__links');
+const nav = document.querySelector('.nav');
 
 const openModal = function (e) {
   e.preventDefault();
@@ -51,7 +56,7 @@ btnScrollTo.addEventListener('click', function (e) {
 //   });
 // });
 
-document.querySelector('.nav__links').addEventListener('click', function (e) {
+navLinks.addEventListener('click', function (e) {
   e.preventDefault();
 
   if (e.target.classList.contains('nav__link')) {
@@ -59,10 +64,6 @@ document.querySelector('.nav__links').addEventListener('click', function (e) {
     document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
   }
 });
-
-const tabs = document.querySelectorAll('.operations__tab');
-const tabsContainer = document.querySelector('.operations__tab-container');
-const tabsContent = document.querySelectorAll('.operations__content');
 
 tabsContainer.addEventListener('click', function (e) {
   const clicked = e.target.closest('.operations__tab');
@@ -78,3 +79,26 @@ tabsContainer.addEventListener('click', function (e) {
   tabsContent.forEach(tc => tc.classList.remove('operations__content--active'));
   content.classList.add('operations__content--active');
 });
+
+const handleHover = function (e) {
+  if (e.target.classList.contains('nav__link')) {
+    const link = e.target;
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+    const logo = link.closest('.nav').querySelector('img');
+
+    siblings.forEach(el => {
+      if (el !== link) el.style.opacity = this;
+    });
+    logo.style.opacity = this;
+  }
+};
+
+nav.addEventListener('mouseover', handleHover.bind(0.5));
+nav.addEventListener('mouseout', handleHover.bind(1));
+
+// nav.addEventListener('mouseover', function (e) {
+//   handleHover(e, 0.5);
+// });
+// nav.addEventListener('mouseout', function (e) {
+//   handleHover(e, 1);
+// });
