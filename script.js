cd41425ac1e01704a9babe64248fa2cbc0ff6930
mmarolin <matija.marolin11@gmail.com>
2022-02-01 20:26:26 +0100

    smooth-scrolling

--- script.js
+++ script.js
@@ -46,3 +46,31 @@ document
     //Old method
     //message.parentElement.removeChild(message);
   });
+
+message.style.backgroundColor = '#37383d';
+message.style.width = '102%';
+
+message.style.height =
+  Number.parseFloat(getComputedStyle(message).height, 10) + 20 + 'px';
+
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click', function (e) {
+  const s1coords = section1.getBoundingClientRect();
+
+  // console.log(s1coords);
+
+  // console.log(e.target.getBoundingClientRect());
+
+  // console.log('Current scroll (x/y)', window.pageXOffset, window.pageYOffset);
+
+  /*
+  window.scrollTo({
+    left: s1coords.left + window.pageXOffset,
+    top: s1coords.top + window.pageYOffset,
+    behavior: 'smooth',
+  });
+*/
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
